526ac55ffe7b86716dcb5a58a6c7e31daac685f1
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-08-25 23:01:54 +0200

    bez tailwindu funguje

--- app-shell/setup.ps1
+++ app-shell/setup.ps1
@@ -1,2 +1,2 @@
-npm i vue@^3.0.0 tailwindcss@2.0.2 postcss-loader@^4.1.0 postcss@^8.2.1 autoprefixer@10.1.0 @babel/preset-typescript@7.12.7 --save
+npm i vue@^3.0.0 postcss-loader@^4.1.0 postcss@^8.2.1 autoprefixer@10.1.0 @babel/preset-typescript@7.12.7 --save
 npm i webpack@5.0.0 webpack-cli@3.3.11 webpack-dev-server@3.11.0 babel-loader@8.1.0 @babel/core@7.9.6 @babel/preset-env@^7.10.3 vue-loader@16.0.0 vue-template-compiler@^2.6.12 html-webpack-plugin@^4.5.0 @vue/compiler-sfc@^3.0.0 ts-loader@^8.0.12 typescript@^4.1.3 style-loader@1.2.1 css-loader@^3.5.3 -D
